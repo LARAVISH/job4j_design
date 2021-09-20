3ca9f608d289abf67cf598aab504822bd48a4000
LARAVISH <fandvor@mail.ru>
2021-09-20 11:39:39 +0500

    SimpleArrayList

---
+++ src/main/java/ru/job4j/tree/Node.java
@@ -0,0 +1,13 @@
+package ru.job4j.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node<E> {
+    final E value;
+    final List<Node<E>> children = new ArrayList<>();
+
+    public Node(E value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,51 @@
+package ru.job4j.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Queue;
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        Node<E> current = new Node<>(child);
+        Node<E> node;
+        List<Node<E>> list = new LinkedList();
+        list.add(root);
+        while (!list.isEmpty()) {
+            node = list.remove(0);
+            if (node.value.equals(parent)
+                    && !node.children.contains(current)) {
+                node.children.add(current);
+                rsl = true;
+                break;
+            }
+            list.addAll(node.children);
+        }
+        return rsl;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}
